2ecb863d8bf8568a772e50ba8a8094806ce4594c
bpepper0822 <bpeppermail@gmail.com>
2018-12-20 21:00:30 -0600

    git commit

--- bool.py
+++ bool.py
@@ -1,6 +1,14 @@
-returns 1 if a > b , 0 if a == b , and -1 if a < b
+# imports the time module for fancy effects using 'time.sleep()'
+import time
 
-def compare(a,b)
+# prints 30 lines
+def clear_screen():
+    print("\n"*30)
+
+# comparison function that all answers run through
+def compare(a,b):
+    print("Comparing ", a, " and ", b, "...")
+    time.sleep(.7)      # Waits 0.7 seconds
     if a > b:
         return 1
     elif a == b:
@@ -8,20 +16,34 @@ def compare(a,b)
     elif a < b:
         return -1
     else:
-        pass
+        return "ERROR: Please enter numeric values only!"      #for typos and anything else that might break
 
-def number_input(n)
-    n = input("Please enter a number to compare\n")
-    return n
-    
-    
-print(compare(5,2)
+# * *__main__ starts here * * 
 
-print(compare(2,5)
+# prints 30 lines
+clear_screen() 
+
+print("Welcome to our number comparison tool!\nWe will compare two numbers.\n")
+
+print("If the first number is larger, I'll display a 1\n")
+
+print("If the second number is larger, I'll display a -1\n")
+
+print("If both numbers are the same, I'll display a 0\n\n")
+
+#user inputs numbers
+num1 = input("Please enter the first number to compare\n")
+num2 = input("Please enter your second number\n")
+
+# prints 30 lines    
+clear_screen()    
+
+print(compare(5,2))
+
+print(compare(2,5))
       
-print(compare(3,3)
+print(compare(3,3))
 
-number_input(a)
-number_input(b)
-compare(a,b)      
+# Recalls the inputs
+print(compare(num1,num2))      
       
