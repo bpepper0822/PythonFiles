fde10f869947450c1bc53e2cab0bd8d68c45a949
bpepper0822 <bpeppermail@gmail.com>
2019-01-18 18:52:38 -0600

    Create dict.py

---
+++ dict.py
@@ -0,0 +1,9 @@
+
+contacts = {
+#     [0] = phone number    [1] = email    [2] = zip code
+'alan' : ['123-456-7890', 'email@email.com', '78260'],
+'bob' : ['234-425-6356', 'email2@email.com', '78258'],
+'catherine' : ['123-745-3463', 'email3@email.com', '77459']
+}
+
+print(contacts['catherine'])
